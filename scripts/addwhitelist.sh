1c22d62dad1c2c27b9f00322d66ab07b5649ce4d
SeeQu <stanislaw.piwowarczyk@golem.network>
2024-09-06 18:07:44 +0200

    update addwhitelist script

--- scripts/addwhitelist.sh
+++ scripts/addwhitelist.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-for i in $(seq 1 25); do echo host$i; docker exec testnet1-provider$i-1 ya-provider whitelist add -p 69.55.231.8; done
+for i in $(seq 1 25); do echo host$i; docker exec devops-testnet-deployment-provider$i-1 ya-provider whitelist add -p 69.55.231.8; done
 
 
